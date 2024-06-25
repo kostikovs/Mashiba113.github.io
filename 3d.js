ce0186e455e3d113646ae120754441b92141141a
Mashiba113 <ruzawink@yandex.ru>
2024-06-25 20:31:40 +0300

    js

---
+++ 3d.js
@@ -0,0 +1,7 @@
+document.addEventListener('logo', function(e) {
+    let dx = e.pageX - window.innerWidth / 2
+    let dy = e.pageY - window.innerHeight / 2
+    let angleX = 20 * dx / window.innerWidth / 2
+    let angleY = 20 * dy / window.innerHeight / 2
+    block.style.transform = `rotateX(${angleX}deg) rotateY(${angleY}deg)`
+})
